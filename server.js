f808feaa3693236cf2d9a77ac33f05f700c2f880
LbnMahs <labanmahs2002@gmail.com>
2024-03-13 05:09:07 +0300

    update(server.js): removing the dummy wallet

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 const connectDb = require("./config/database");
-const IntaSendService = require("./config/intasendService");
-
-const intaSendService = new IntaSendService(
-  process.env.PUBLICKEY,
-  process.env.PRIVATEKEY
-);
 
 connectDb();
 
@@ -23,22 +17,3 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-// dummy wallet
-intaSendService
-  .createWallet({
-    label: "Unique Test Wallet",
-    wallet_type: "WORKING",
-    currency: "KES",
-    can_disburse: false,
-  })
-  .then((response) => {
-    console.log("Intasend service is up and running");
-  })
-  .catch((error) => {
-    // Convert the Buffer to a string
-    const errorMessage = error.toString();
-    console.error(
-      "There was an error with the Intasend service:",
-      errorMessage
-    );
-  });
