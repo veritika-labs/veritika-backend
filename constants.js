ae61e83c0b783df1b3129b658fdfa2aa352abd7d
SamsonMokaya <mokayasamson950@gmail.com>
2024-05-05 09:08:06 +0300

    Fix - Toke  error

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ exports.constants = {
     VALIDATION_ERROR: 400,
     NOT_FOUND: 404,
     SERVER_ERROR: 500,
-    UNAUTHORIZED: 501,
+    UNAUTHORIZED: 401,
     FORBIDDEN: 403,
     OK: 200,
     CREATED: 201,
